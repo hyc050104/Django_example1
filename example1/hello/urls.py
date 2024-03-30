3e4e9654c37262df010a4de81bae6381a4ed0514
hyclocal1 <207099935@qq.com>
2024-03-30 21:10:21 +0800

    hello1.0

---
+++ example1/hello/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("", views.index, name = "index"),
+    path("<str:name>", views.greet, name = "greet"),
+    path("foo", views.foo, name = "foo")
+]
