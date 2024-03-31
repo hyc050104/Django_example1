ceb854eff97c141027443189fcd2f013295e7bc4
hyclocal1 <207099935@qq.com>
2024-03-31 17:52:36 +0800

    task1.0

--- example1/tasks/views.py
+++ example1/tasks/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django import forms
 
+tasks = ["foo", "bar", "baz"]
+prior = ["1", "3", "5"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 # Create your views here.
+
+def index(request):
+    return render(request, "tasks/index.html", {"tasks":tasks,"priority":prior})
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            priority = form.cleaned_data["priority"]
+            prior.append(priority)
+        else:
+            return render(request, "tasks/add.html", {"form" : form})
+    return render(request, "tasks/add.html", {"form" : NewTaskForm()})
